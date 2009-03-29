4a5eb33aaa22e8321540d8405aa1b41c386ba9bc
kosmas58 <kosmas.schuetz@gmx.com>
2009-03-29 10:27:41 +0200

    Updated build #.
    Signed-off-by: kosmas58 <kosmas.schuetz@gmx.com>

--- lib/cruise_control/version.rb
+++ lib/cruise_control/version.rb
@@ -4,7 +4,7 @@ module CruiseControl
       MAJOR = 1
       MINOR = 3
       TINY  = 7
-      PATCH =  (/[0-9]+/.match("$Rev: 5935 $"))[0]    
+      PATCH =  (/[0-9]+/.match("$Rev: 5944 $"))[0]    
       STRING = [MAJOR, MINOR, TINY, PATCH].compact.join('.')
     end
   end
