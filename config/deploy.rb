c229c89e1528f8323b7c45983e95b9253f94b196
Andrew Roth <andrewroth@gmail.com>
2009-05-15 00:15:00 -0400

    use ministryapp insead of c4c

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,16 @@
 set :application, "cruisecontrol.rb"
 set :user, 'deploy'
-set :use_sudo, false
-set :host, "pat.powertochange.org"
+set :use_sudo, true
+set :host, "ministryapp.com"
 
 set :scm, "git"
 set :repository, "git://github.com/andrewroth/#{application}.git"
 set :deploy_via, :remote_cache
-set :deploy_to, "/var/www/cc.campusforchrist.org"
+set :deploy_to, "/var/www/cc.ministryapp.com"
 set :git_enable_submodules, false
 set :git_shallow_clone, true
+
+ssh_options[:port] = 40022
 
 server host, :app, :web, :db, :primary => true
 after "deploy", "deploy:cleanup"
